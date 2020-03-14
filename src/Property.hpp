8b851f90db497dd1a9bf56192371de079699056e
Simon Reber <sr.dev@outlook.com>
2020-03-14 14:24:43 +0100

    propertys constructors extended

--- src/Property.hpp
+++ src/Property.hpp
@@ -8,7 +8,7 @@ using std::function;
 using namespace std::placeholders; // for _1, _2, ...
 
 /**
- * @brief utility class to emulate the read only property functionality of .NET
+ * @brief utility class to emulate the read only Property functionality of .NET
  * @tparam T The type of the Property
  */
 template <typename T>
@@ -16,30 +16,62 @@ struct PropertyR
 {
     using Getter = function<T(void)>;
 
-    // PropertyR(aValueImpl<T> &value) {
-    //     auto a = bind(&aValueImpl<T>::get_value, value);
-    // }
+    /**
+     * @brief Construct a new reaf only Property object
+     * 
+     * The constructor installs a getter lambda that
+     * operates on a reference of the passed variable     * 
+     * 
+     * @param value 
+     */
     PropertyR(T &value) : getter_([&value]() -> T {
                               return value;
                           }) {}
+
     PropertyR(Getter getter) : getter_(getter) {}
+
     operator T const() { return getter_(); }
+
+    
+    template <class... _Args>
+	void operator()(_Args &&... _Ax)
+	{
+		getter_()(forward<_Args>(_Ax)...);
+	}
+
 
 protected:
     Getter getter_;
 };
 /**
- * @brief utility class to emulate the property functionality of .NET
+ * @brief utility class to emulate the Property functionality of .NET
  * @tparam T The type of the Property
  */
 template <typename T>
 struct PropertyRW : public PropertyR<T>
 {
-    using Getter = typename PropertyR<T>::Getter;
-    using Setter = function<void(T newValue)>;
-    PropertyRW(T & value) : PropertyR<T>(value), setter_([&value](T v) {
-                                value = v;
-                            }) {}
+    using Getter = typename PropertyR<T>::Getter;   ///> Prototype from getter function
+    using Setter = function<void(T newValue)>;      ///> Protoype of setter function
+
+    /**
+     * @brief Construct a new read/write Property object
+     * 
+     * The constructor installs a setter lambda that
+     * operates on a reference of the passed variable     * 
+     * 
+     * @param value 
+     */
+    PropertyRW(T &value) : PropertyR<T>(value), setter_([&value](T v) {
+                               value = v;
+                           }) {}
+
+
+    PropertyRW(Getter getter, T &value) : PropertyR<T>(getter), setter_([&value](T v) {
+                               value = v;
+                           }) {}
+                           
+    PropertyRW(T & value, Setter setter) : PropertyR<T>(value), setter_(setter) {}
+    
     PropertyRW(Getter getter, Setter setter) : PropertyR<T>(getter), setter_(setter) {}
 
     PropertyRW &operator=(T &&value)
