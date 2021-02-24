99ca6912e5230e02626c68fb76f00b45149122b9
Simon Reber <sr.dev@outlook.com>
2021-02-24 23:19:46 +0100

    update to latest doc

--- docs/struct_control_1_1_control_manager.js
+++ docs/struct_control_1_1_control_manager.js
@@ -4,8 +4,8 @@ var struct_control_1_1_control_manager =
     [ "ControlManager", "struct_control_1_1_control_manager.html#ae5f31a97af064123b199a0604b621862", null ],
     [ "~ControlManager", "struct_control_1_1_control_manager.html#ab4bf17beec924128111c44c236e3cdc7", null ],
     [ "get", "struct_control_1_1_control_manager.html#ac12d50b6c23e1c2feea0474a6a733828", null ],
-    [ "operator[]", "struct_control_1_1_control_manager.html#acf48e267841bd1f671cf1b33eb34240a", null ],
-    [ "operator[]", "struct_control_1_1_control_manager.html#a01d8b59f6b4f2358227c66aaccab3897", null ],
+    [ "operator[]", "struct_control_1_1_control_manager.html#acbca96e7415fc0c4d5744335b4fd10bd", null ],
+    [ "operator[]", "struct_control_1_1_control_manager.html#aefb6ca79d07635f810cd9fad0294c88d", null ],
     [ "Control", "struct_control_1_1_control_manager.html#a9adf7c444a8611024541177662d4716a", null ],
     [ "pImpl_", "struct_control_1_1_control_manager.html#aca895997c96bb4e0ea1950cda859b707", null ]
 ];
